7f87d01f0c0a31d9c59558bd7e2ea96a54f4242c
sociofriend <lilit_avdalyan@yahoo.com>
2025-07-24 00:12:48 +0400

    v1: pushed to app store connect; ref: minor ui changes

---
+++ Kill The Cockroach/Kill_The_CockroachExtension.swift
@@ -0,0 +1,13 @@
+//
+//  Kill_The_CockroachExtension.swift
+//  Kill The Cockroach
+//
+//  Created by Lilit Avdalyan on 23.07.25.
+//
+
+import AppIntents
+import ExtensionFoundation
+
+@main
+struct Kill_The_CockroachExtension: AppIntentsExtension {
+}
